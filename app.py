ee874daa632fbc02ba6ea99dbdba4aea36c30e3b
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2016-03-13 10:11:53 -0400

    Add foursquare button.

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+
+import flask
+
+# app = Flask(__name__, static_url_path='/static', static_folder='static/')
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+	return flask.render_template('index.html')
+
+# @app.route('/img/<path:path>')
+# def send_img(path):
+# 	return flask.send_from_directory('static/img', path)
+
+if __name__ == '__main__':
+    app.debug = True
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
