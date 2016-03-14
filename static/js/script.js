18e59f8002102e6a180d0045647d363fa1b01ffd
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2016-03-13 20:52:52 -0400

    Plot activity from multiple sources.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
-function visualize(timestamps) {
+var Service = Object.freeze({FACEBOOK: 0, TWITTER: 1, GITHUB: 2, FOURSQUARE: 3});
+
+function visualize(timestamps, service) {
 
   var margin = {top: 10, right: 30, bottom: 30, left: 30},
     width = 960 - margin.left - margin.right,
@@ -35,23 +37,42 @@ function visualize(timestamps) {
     .append('g')
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+  var barClass = "bar";
+  switch (service) {
+    case Service.FACEBOOK:
+      barClass += " fb";
+      break;
+    case Service.TWITTER:
+      barClass += " tw";
+      break;
+    case Service.GITHUB:
+      barClass += " gh";
+      break;
+    case Service.FOURSQUARE:
+      barClass += " fsq";
+      break;
+    default:
+      break;
+  }
+
   var bar = svg.selectAll('.bar')
       .data(data)
     .enter().append('g')
-      .attr('class', "bar")
+      .attr('class', barClass)
       .attr('transform', function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
   
   bar.append("rect")
-    .attr("x", 1)
-    .attr("width", x(data[0].dx) - 1)
+    .attr("x", x(data[0].dx) / 4 * service)
+    .attr("width", x(data[0].dx) / 4)
     .attr("height", function(d) { return height - y(d.y); });
 
-  bar.append("text")
-    .attr("dy", ".75em")
-    .attr("y", 6)
-    .attr("x", x(data[0].dx) / 2)
-    .attr("text-anchor", "middle")
-    .text(function(d) { return d.y; });
+  // bar.append("text")
+  //   .attr("dy", ".75em")
+  //   .attr("y", 6)
+  //   .attr("x", x(data[0].dx) / 2)
+  //   .attr("text-anchor", "middle")
+  //   .text(function(d) { return d.y; });
 
   svg.append("g")
     .attr("class", "x axis")
@@ -59,8 +80,12 @@ function visualize(timestamps) {
     .call(xAxis);
 }
 
-function requestFoursquareData() {
-  $.getJSON('/fsq_data', function( data ) {
-    visualize(data);
+function requestTimestamps() {
+  $.getJSON('/fsq_data', function(data) {
+    visualize(data, Service.FOURSQUARE);
+  });
+
+  $.getJSON('/gh_data', function(data) {
+    visualize(data, Service.GITHUB);
   });
 }
