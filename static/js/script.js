46979caad972586f01a36587d67080c0f3f9a39a
Huai-Che (Acsa) Lu <hc.acsalu@gmail.com>
2016-03-15 23:57:35 -0400

    Show loading progress icon.

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,9 @@
 function requestTimestamps() {
+
   $.getJSON('/fb_data', function(data) {
     activities[Service.FACEBOOK] = data;
     $('.btn-facebook+p').html(data.length + " Posts & Likes").show();
+    showCheckMark($('.btn-facebook'));
     visualize();
   });
 
@@ -9,20 +11,31 @@ function requestTimestamps() {
     activities[Service.INSTAGRAM] = data;
     var stats = 
     $('.btn-instagram+p').html(data.length + " Photos").show();
+    showCheckMark($('.btn-instagram'));
     visualize();
   });
 
   $.getJSON('/gh_data', function(data) {
     activities[Service.GITHUB] = data;
     $('.btn-github+p').html(data.length + " Events").show();
+    showCheckMark($('.btn-github'));
     visualize();
   });
 
   $.getJSON('/fsq_data', function(data) {
     activities[Service.FOURSQUARE] = data;
     $('.btn-foursquare+p').html(data.length + " Check-ins").show();
+    showCheckMark($('.btn-foursquare'));
     visualize();
   });
+}
+
+function showCheckMark($btn) {
+  $btn.find('.progress-indicator')
+      .toggleClass("fa-circle-o-notch")
+      .toggleClass("fa-spin")
+      .toggleClass("fa-check")
+      .show();
 }
 
 function clearCanvas() {
